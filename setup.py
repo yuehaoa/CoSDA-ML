5e6818020aea445f50ddadd3058b6491affadaeb
yuehaoa <53177683+yuehaoa@users.noreply.github.com>
2022-02-22 16:17:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
         "torch==1.1.0",
         "numpy==1.17.2",
         "tqdm==4.36.1",
-        "transformer==2.3.0",
+        "transformers==2.3.0",
         "pytorch-transformers==1.2.0",
     ]
 )
