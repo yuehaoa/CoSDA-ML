8fcd36837963b86f5a369706c5d67c1b466c7c74
yuehaoa <53177683+yuehaoa@users.noreply.github.com>
2022-02-22 16:04:32 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    install_requires=[
+        "python==3.7.4",
+        "torch==1.1.0",
+        "numpy==1.17.2",
+        "tqdm==4.36.1",
+        "transformer==2.3.0",
+        "pytorch_transformers==1.2.0",
+    ]
+)
